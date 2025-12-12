f4fa3f9e8f253f02204e42ebcade6ff27a87ef90
Subham0803a <subhampratap0@gmail.com>
2025-12-13 00:24:41 +0530

    setup base server file

--- server/main.py
+++ server/main.py
@@ -5,9 +5,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "testing Hello World"}
+    return {"message": "FastAPI Hello World"}
 
 if __name__ == "__main__":
     # The uvicorn.run command starts the server
     # --port 5000 sets the server to run on port 5000 (as requested)
-    uvicorn.run("main:app", host="0.0.0.0", port=4000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
