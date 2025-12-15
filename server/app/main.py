7696e04a531c147472f15d2520266415050d97a3
Subham0803a <subhampratap0@gmail.com>
2025-12-16 01:23:29 +0530

    update file structure & server

--- server/app/main.py
+++ server/app/main.py
@@ -7,7 +7,11 @@ app = FastAPI()
 def read_root():
     return {"message": "FastAPI Hello World"}
 
+@app.get("/root")
+def testing():
+    return {"message": "This is root endpoint"}
+
 if __name__ == "__main__":
     # The uvicorn.run command starts the server
-    # --port 5000 sets the server to run on port 5000 (as requested)
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
+    # --port 5000 sets the server to run on port 5000
+    uvicorn.run("main:app", host="localhost", port=5000, reload=True)
